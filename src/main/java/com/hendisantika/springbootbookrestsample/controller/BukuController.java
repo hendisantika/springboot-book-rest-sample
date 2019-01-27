47d300b6afe857ad75cfdc4765d38eb727e554d7
Hendi Santika <hendisantika@gmail.com>
2019-01-27 21:40:13 +0700

    #7 - Add Buku Controller

---
+++ src/main/java/com/hendisantika/springbootbookrestsample/controller/BukuController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootbookrestsample.controller;
+
+import com.hendisantika.springbootbookrestsample.repository.BukuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-book-rest-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-27
+ * Time: 21:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+@RequestMapping("/buku")
+public class BukuController {
+    @Autowired
+    BukuRepository bukuRepository;
+}
