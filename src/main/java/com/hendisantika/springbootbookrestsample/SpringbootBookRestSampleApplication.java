864049cc5f43d572fccb28db4309498978604789
Hendi Santika <hendisantika@gmail.com>
2019-01-27 21:24:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootbookrestsample/SpringbootBookRestSampleApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootbookrestsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookRestSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBookRestSampleApplication.class, args);
+    }
+
+}
+
