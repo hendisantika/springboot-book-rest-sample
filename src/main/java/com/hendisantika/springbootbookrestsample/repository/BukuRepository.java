d1aaa1c9855210e97160fe79e6357ef36b051827
Hendi Santika <hendisantika@gmail.com>
2019-01-27 21:37:42 +0700

    #6 - Add Buku Repository

---
+++ src/main/java/com/hendisantika/springbootbookrestsample/repository/BukuRepository.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootbookrestsample.repository;
+
+import com.hendisantika.springbootbookrestsample.model.Buku;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-book-rest-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-27
+ * Time: 21:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface BukuRepository extends JpaRepository<Buku, Long> {
+    List<Buku> findByStatusPeminjaman(int statusPeminjaman);
+
+    List<Buku> findByTitleBook(String titleBook);
+}
