910f123b6d9765767cc138300aaf20e8b15e9060
Hendi Santika <hendisantika@gmail.com>
2019-01-27 21:53:39 +0700

    #10 - Update Data Buku in  Buku Controller

--- src/main/java/com/hendisantika/springbootbookrestsample/controller/BukuController.java
+++ src/main/java/com/hendisantika/springbootbookrestsample/controller/BukuController.java
@@ -3,10 +3,12 @@ package com.hendisantika.springbootbookrestsample.controller;
 import com.hendisantika.springbootbookrestsample.model.Buku;
 import com.hendisantika.springbootbookrestsample.repository.BukuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,4 +37,32 @@ public class BukuController {
     public Buku tambahBuku(@Valid @RequestBody Buku buku) {
         return bukuRepository.save(buku);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Buku> updateBuku(@PathVariable(value = "id") Long id, @Valid @RequestBody Buku detailBuku) {
+
+        Optional<Buku> buku = bukuRepository.findById(id);
+
+        if (buku.isPresent()) {
+
+            Buku dataBuku = buku.get();
+            dataBuku.setTitleBook(detailBuku.getTitleBook());
+
+            dataBuku.setNamaDepanPengarang(detailBuku.getNamaDepanPengarang());
+
+            dataBuku.setNamaBelakangPengarang(detailBuku.getNamaBelakangPengarang());
+
+            dataBuku.setNamaPeminjam(detailBuku.getNamaPeminjam());
+
+            dataBuku.setStatusPeminjaman(detailBuku.getStatusPeminjaman());
+
+            Buku updatedBuku = bukuRepository.save(buku);
+
+            return ResponseEntity.ok(updatedBuku);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
